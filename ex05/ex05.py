8090542fd01383af4184637cfd5a06208efd180a
KimhyungUk <https://github.com/kimhyunguk/16PFC-kimhyunguk.git>
2016-03-29 11:44:25 +0900

    study drills

---
+++ ex05/ex05.py
@@ -0,0 +1,18 @@
+my_name = 'Hyunguk Kim'
+my_age = 26
+my_height_cm = 180.5
+cm_to_inch = 1.0 / 2.54
+my_height_inch = my_height_cm * cm_to_inch
+my_weight_kg = 56.1
+my_eyes = 'black'
+my_teeth = 'White depending on coffee'
+my_hair = 'black'
+
+print "Let's talk about %s." % my_name
+print "He's %g centimeters tall." % my_height_cm
+print "He's %g inches tall." % my_height_inch
+print "He's %d kilograms heavy." % my_weight_kg
+print "He's %g kilograms heavy." % my_weight_kg
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
