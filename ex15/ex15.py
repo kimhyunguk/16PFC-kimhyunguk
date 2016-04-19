bc9a7a0dd764622d1225aceae5ec55f4cb600932
CAD Client <CAD Client>
2016-04-19 11:03:25 +0900

    ex16.py

--- ex15/ex15.py
+++ ex15/ex15.py
@@ -8,8 +8,8 @@ print "Here's your file %r:" % filename
 print txt.read()
 
 print "Type the filename again:"
-file_again = raw_input(">")
+file_again = raw_input("> ")
 
-txt_again = raw_input(file_again)
+txt_again = open(file_again)
 
 print txt_again.read()
